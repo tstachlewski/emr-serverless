8f1078d91d320729816e5e68a353365e950667d2
EC2 Default User <ec2-user@ip-172-31-37-207.eu-west-1.compute.internal>
2024-01-20 17:43:57 +0000

    ok

--- data-converter.py
+++ data-converter.py
@@ -1,13 +1,25 @@
 import os
 import sys
-import boto3
+from pyspark.sql import SparkSession
 
-bucket = "tomke-demo-emr"
-key = "data/people-input/people-02.csv"
 
-index = key.rfind("/") + 1
+if __name__ == "__main__":
 
-file = key[key.rfind("/") + 1:]
+    bucket = sys.argv[1]
+    key = sys.argv[2]
+    
+    file_name = key[key.rfind("/") + 1:]
+    data_source = "s3://" + bucket + "/" + key
+    output_uri = "s3://" + bucket + "/data/people-parquet"
 
-print(index)
-print(file)
+    print(f"Data Source: {data_source}")
+    print(f"File Name: {file_name}")
+    print(f"Output Location: {output_uri}")
+
+    #Conveting data from CSV to PARQUET
+    print("Conveting data from CSV to PARQUET")
+    spark = SparkSession.builder.appName("People-DataConverter").getOrCreate()
+    spark.conf.set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
+    df = spark.read.option("header","true").csv(data_source)
+    df.write.mode("append").parquet(output_uri)
+    spark.stop()
